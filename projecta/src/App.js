49d63998f06f6a671d95691e474782f5cdf237f6
dawidekk523 <daveedsgn@gmail.com>
2023-04-03 17:16:09 +0200

    1.1

--- projecta/src/App.js
+++ projecta/src/App.js
@@ -28,9 +28,43 @@ const guyInfo = guy.map((guy) => {
 });
 
 function App() {
-  return <div>{guyInfo}</div>;
+  return (
+    <div>
+      {guyInfo}
+      <Input />
+    </div>
+  );
+}
+function inputAlert(e) {
+  console.log(e.target.value);
+}
+function addNewGuy(e) {
+  e.preventDefault();
+  console.log("dodaje");
 }
 
+function Input() {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col">
+        <form onSubmit={addNewGuy}>
+          <p className="text-[#b7b5fd]">Twoje aktualne stanowisko</p>
+          <input
+            onChange={inputAlert}
+            className="border-2 border-gray-300 focus:outline-none focus:border-[#3e20b2] rounded"
+            placeholder=""
+          />
+          <button
+            type="submit"
+            className="bg-[#361d95] rounded-xl p-2 text-white mx-2 px-8 hover:bg-[#4c28d9]"
+          >
+            submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 function OneItem(props) {
   const { job, age } = props.guy;
   return (
